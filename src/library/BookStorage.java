e880e1d3efeed8f9270ef2452863159e3fdb79d6
Samvel Torosyan <samveltorosyan29@gmail.com>
2025-10-18 00:30:25 +0400

    Library

---
+++ src/library/BookStorage.java
@@ -0,0 +1,51 @@
+package library;
+
+public class BookStorage {
+
+    private int size;
+    private Book[] books = new Book[10];
+
+    public void add(Book book) {
+
+        if (size == books.length) {
+
+            extend();
+        }
+
+        books[size++] = book;
+
+    }
+
+    private void extend() {
+
+        Book[] tmp = new Book[size + 10];
+        System.arraycopy(books, 0, tmp, 0, size);
+        books = tmp;
+
+    }
+
+    public void print() {
+
+        for (int i = 0; i < size; i++) {
+
+            System.out.println(books[i]);
+
+        }
+
+    }
+
+    public void search(String keyword) {
+
+        for (int i = 0; i < size; i++) {
+
+            if(books[i].getTitle().toLowerCase().contains(keyword.toLowerCase())){
+
+                System.out.println(books[i]);
+
+            }
+
+        }
+
+
+    }
+}
